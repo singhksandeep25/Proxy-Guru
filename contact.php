4a63f8518b172c60826a002c4e0fdfce937929f6
Sandeep Kumar Singh <singh_sandeep1998@hotmail.com>
2016-04-03 10:19:14 +0530

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  <!--<style> #bar{ font-family: "Roboto", sans-serif;}</style>-->
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="msapplication-tap-highlight" content="no">
+    <meta name="description" content="Materialize is a modern responsive CSS framework based on Material Design by Google. ">
+    <title>Profile</title>
+    <!-- Favicons-->
+   <link rel="icon" href="images/new.png" sizes="32x32">
+    <meta name="msapplication-TileColor" content="#FFFFFF">
+    <meta name="msapplication-TileImage" content="images/favicon/mstile-144x144.png">
+    <link rel="icon" href="images/favicon/favicon-32x32.png" sizes="32x32">
+    <!--  Android 5 Chrome Color-->
+    <meta name="theme-color" content="#EE6E73">
+    <!-- CSS-->
+
+    <link rel="stylesheet" href="css/normalize.css">
+
+    <link rel="stylesheet" href="css/style2.css">
+
+
+    <link href="css/prism.css" rel="stylesheet">
+    <link href="css/ghpages-materialize.css" type="text/css" rel="stylesheet" media="screen,projection">
+    <link href="http://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <script>
+      window.liveSettings = {
+        api_key: "a0b49b34b93844c38eaee15690d86413",
+        picker: "bottom-right",
+        detectlang: true,
+        dynamic: true,
+        autocollect: true
+      };
+    </script>
+    <script src="//cdn.transifex.com/live.js"></script>
+  </head>
+  <body>
+    <header>
+      <nav class="top-nav" >
+        <div class="container">
+          <div class="nav-wrapper"><a href = "home.php" class="page-title">Home</a></div>
+        </div>
+      </nav>
+      <div class="container"><a href="#" data-activates="nav-mobile" class="button-collapse top-nav full hide-on-large-only"><i class="material-icons">menu</i></a></div>
+      <ul id="nav-mobile" class="side-nav fixed">
+
+      <img src ="images/new.png" height="225" width="225" >
+        
+       <!-- <a id = "bar" style="font-family: Roboto, sans-serif">-->
+
+
+        <li class="bold"><a class=fo href="home.php" class="waves-effect waves-teal">Home</a></li>
+        <li class="bold"><a href="marketplace.php" class="waves-effect waves-teal">Marketplace</a></li>
+        
+        <li class="bold"><a href="myrequests.php" class="waves-effect waves-teal">My Requests</a></li>
+        <li class="bold"><a href="contact.php" class="waves-effect waves-teal">Contact Us</a></li>
+      </ul>
+
+      </a>
+    </header>
+    <main>
+
+ 
+    <h1></h1>
+<section>
+
+
+
+<footer class="page-footer black-we-want">
+    <div class="container">
+      <div class="row">
+        <div class="col l6 s12">
+          <h2 class="white-text">Contact Us</h2>
+          <p class="white-text" >For hugs and bugs, shoot an email to any of us: </p>
+          <p class="white-text" >Abhinav Khare : iit2015036@iiita.ac.in</p>
+<p class="white-text">Sandeep Kumar Singh : iit2015014@iiita.ac.in</p>
+<p class="white-text">Shubham Padia : iit2015074@iiita.ac.in</p>
+</div>
+        
+
+      </div>
+    </div>
+    
+  </footer>
+
+
+
+            
+    <!--  Scripts-->
+    <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+    <script>if (!window.jQuery) { document.write('<script src="bin/jquery-2.1.1.min.js"><\/script>'); }
+    </script>
+    <script src="js/jquery.timeago.min.js"></script>
+    <script src="js/prism.js"></script>
+    <script src="jade/lunr.min.js"></script>
+    <script src="jade/search.js"></script>
+    <script src="bin/materialize.js"></script>
+    <script src="js/init.js"></script>
+    <!-- Twitter Button -->
+    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+
+    <!-- Google Plus Button-->
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
+
+    <!--  Google Analytics  -->
+    <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-56218128-1', 'auto');
+    ga('require', 'displayfeatures');
+    ga('send', 'pageview');
+    </script>
+
+  </body>
+</html>
